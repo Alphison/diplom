ef6b74daa3639279baa203d9c34b9d98ed3a216d
Alphison <alfismusin13@gmail.com>
2023-03-03 21:19:20 +0300

    first commit backend

---
+++ backend/database/migrations/2023_03_03_171504_create_courses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('data');
+            $table->integer('count_lesson');
+            $table->foreignId('id_prepod')->constrained();
+            $table->string('profession');
+            $table->string('img_course');
+            $table->string('goal');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+};
